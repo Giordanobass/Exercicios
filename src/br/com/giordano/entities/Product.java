e35b660fcd69726171be8871793027343224219a
Giordanobass <giordanoamaral@gmail.com>
2019-12-20 18:41:20 -0300

    Exercicios aleatorios

---
+++ src/br/com/giordano/entities/Product.java
@@ -0,0 +1,38 @@
+package br.com.giordano.entities;
+
+public class Product {
+
+	private String name;
+	private Double price;
+
+	public Product() {
+	}
+
+	public Product(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public String priceTag() {
+		return name + " $ "
+				+ String.format("%.2f", price);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
